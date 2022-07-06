8ecec24174acfd1ef9d3766b511d00c0cbb65583
uzair-anwar <uzair.anwar@devsinc.com>
2022-07-06 19:25:59 +0500

    Changed variable names, adjusted abnormal formatting and resolved higlighted issues like invalid expressions on use of variable, parenthesis issues and negative value issues etc

--- custom.js
+++ custom.js
@@ -1,182 +1,187 @@
-var screen = document.querySelector('#screen');
-var ans_screen = document.querySelector('#ansscreen')
-var btn = document.querySelectorAll('.btn');
+let inputValue = document.querySelector("#input-screen");
+let outputValue = document.querySelector("#output-screen");
+let button = document.querySelectorAll(".btn");
 let history_element = document.querySelector(".memory");
-let show_variables = document.querySelector(".show-var")
+let show_variables = document.querySelector(".show-var");
 let historySection = document.querySelector(".var-history");
 let errorSection = document.querySelector("#error");
-var showError=false;
-var historybtn=false;
-var showvarbtn=false;
-var variables=new Map();
+let showError = false;
+let historyButton = false;
+let showVarBtn = false;
+let variables = new Map();
 /*============ For getting the value of btn, Here we use for loop ============*/
-for (item of btn) {
-    item.addEventListener('click', (e) => {
-            btntext = e.target.value;
-            screen.value += btntext;
-            e.target.blur();
-            screen.focus();
-    });
+for (item of button) {
+  item.addEventListener("click", (e) => {
+    btntext = e.target.value;
+    inputValue.value += btntext;
+    e.target.blur();
+    inputValue.focus();
+  });
+}
+
+//function to remove values from both fields
+function remove() {
+  inputValue.value = "";
+  outputValue.value = "";
 }
 
 //function for removing a element
-function backspc() {
-    screen.value = screen.value.substr(0, screen.value.length - 1);
+function backSpace() {
+  inputValue.value = inputValue.value.substr(0, inputValue.value.length - 1);
 }
 
 //function for solving equation
-function solve(){
-    try {  
-    const PI="3.1415";
-    const E="2.7182";
-    ans_screen.value=""
-    var expression=screen.value;
+function solve() {
+  try {
+    const PI = "3.1415";
+    const E = "2.7182";
+    outputValue.value = "";
+    var expression = inputValue.value;
 
+    if (expression.length == 0) {
+      throw new Error("Empty");
+    }
     for (const [key, value] of variables) {
-        let re = new RegExp(`\\b${key}\\b`, "gi");
-        expression=expression.replace(key,value)  
+      let dynamicReg = new RegExp(`\\b${key}\\b`, "gi");
+      expression = expression.replace(dynamicReg, value);
     }
-    if(expression.includes("eπ") || expression.includes("πe")){
-        throw new Error("NaN")
+    if (expression.includes("eπ") || expression.includes("πe")) {
+      throw new Error("NaN");
     }
-    expression=expression.replace("π",PI)
-    expression=expression.replace("e",E);
-    console.log(expression)
-    result=evaluate(expression);
-    if(result == "Infinity")
-    {
-        throw new Error("Infinity");
+    let dynamicRegE = new RegExp(`\\b${"e"}\\b`, "gi");
+    expression = expression.replace("π", PI);
+    expression = expression.replace(dynamicRegE, E);
+    if (/[a-z]/i.test(expression) && /[A-Z]/i.test(expression)) {
+      throw new Exception("NaN");
     }
-    else if(result.toString()=="NaN" || result=="Error"){
-        throw new Error("NaN")
+    result = evaluate(expression);
+    if (result == "Infinity") {
+      throw new Error("Infinity");
+    } else if (result.toString() == "NaN" || result == "Error") {
+      throw new Error("NaN");
     }
-        
-    finalResult=parseFloat(result);
-    finalResult=Math.round(finalResult*10000)/10000;
-    ans_screen.value=finalResult;
 
-    history_element.innerHTML+= `
-    <div><button id="${expression}" value="${finalResult}" class"historybtn" name="${expression}">
+    finalResult = parseFloat(result);
+    finalResult = Math.round(finalResult * 10000) / 10000;
+    outputValue.value = finalResult;
+
+    history_element.innerHTML += `
+    <div><button id="${expression}" value="${finalResult}" class"historyButton" name="${expression}">
     ${expression + " = " + finalResult}
     </button>
     <button id="${expression}" name="delete">Delete</button>
     </div>`;
-    } catch (Exception) {
-        if(Exception.message=="Infinity")
-            ans_screen.value="Second number is zero";
-        else
-            ans_screen.value="Expression is not valid";
-    }
+  } catch (Exception) {
+    if (Exception.message == "Infinity")
+      outputValue.value = "Second number is zero";
+    else if (Exception.message == "Empty")
+      outputValue.value = "Input Value is Empty";
+    else outputValue.value = "Expression is not valid";
+  }
 }
 
 //function for creating variables
-function createVar(){
-    let key=document.getElementById("varkey").value;
-    let targetvalue=document.getElementById("varvalue").value;
-    try{
-        if(!isNaN(key) && key!=""){
-            throw new Error("Key can not be a Number")
-        }
-
-        else if(key=="" && targetvalue==""){
-            errorSection.style.display="block";
-            document.getElementById("error").value="Fields are empty"
-        }
-        else if(isNaN(targetvalue)){
-            throw new Error("Value is not a Number")
-        }
-        else if (
-            key.toLowerCase() == "tan" ||
-            key.toLowerCase() == "cos" ||
-            key.toLowerCase() == "sin" ||
-            key.toLowerCase() == "sqrt" ||
-            key.toLowerCase() == "pi" ||
-            key.toLowerCase() == "e"
-          ){
-            errorSection.style.display="block";
-            document.getElementById("error").value="Reserve variable cann't be declare"
-          }
-        else if(variables.has(key)){
-            errorSection.style.display="block";
-            document.getElementById("error").value="=variable is already declare"
-            
-        }else{
-                variables.set(key,targetvalue);
-                document.getElementById("varkey").value=""
-                document.getElementById("varvalue").value=""
-                show_variables.innerHTML+=`
-                <div><button id=${key} value=${targetvalue} name="putvar">${key +" = "+targetvalue}</button>
+function createVariable() {
+  let key = document.getElementById("variable-key").value;
+  let targetValue = document.getElementById("variable-value").value;
+  try {
+    if (!isNaN(key) && key != "") {
+      throw new Error("Key can not be a Number");
+    } else if (key == "" && targetValue == "") {
+      errorSection.style.display = "block";
+      document.getElementById("error").value = "Fields are empty";
+    } else if (isNaN(targetValue)) {
+      throw new Error("Value is not a Number");
+    } else if (
+      key.toLowerCase() == "tan" ||
+      key.toLowerCase() == "cos" ||
+      key.toLowerCase() == "sin" ||
+      key.toLowerCase() == "sqrt" ||
+      key.toLowerCase() == "pi" ||
+      key.toLowerCase() == "e"
+    ) {
+      errorSection.style.display = "block";
+      document.getElementById("error").value =
+        "Reserve variable can't be declare";
+    } else if (variables.has(key)) {
+      errorSection.style.display = "block";
+      document.getElementById("error").value = "Variable is already declared";
+    } else {
+      variables.set(key, targetValue);
+      document.getElementById("variable-key").value = "";
+      document.getElementById("variable-value").value = "";
+      show_variables.innerHTML += `
+                <div><button id=${key} value=${targetValue} name="putvar">${
+        key + " = " + targetValue
+      }</button>
                 <button id=${key} name="deletevar">Delete</button>
-                </div>`
-                errorSection.style.display="block";
-                document.getElementById("error").value="Variable is created successfully!";
-    
-        }
-    } catch(Exception)
-    {
-        errorSection.style.display="block";
-        document.getElementById("error").value=Exception.message;
+                </div>`;
+      errorSection.style.display = "block";
+      document.getElementById("error").value =
+        "Variable is created successfully!";
+      show_variables.style.display = "block";
+      showVarBtn = true;
     }
+  } catch (Exception) {
+    errorSection.style.display = "block";
+    document.getElementById("error").value = Exception.message;
+  }
 }
 
 //function for showning histroy
-function history(){
-    if (!historybtn) {
-        historySection.style.display = "block";
-        historybtn = true;
-      } else {
-        historySection.style.display = "none";
-        historybtn = false;
-      }
+function showHistory() {
+  if (!historyButton) {
+    historySection.style.display = "block";
+    historyButton = true;
+  } else {
+    historySection.style.display = "none";
+    historyButton = false;
+  }
 }
 
 //Event for Enter or backspace
-screen.addEventListener("keydown", function (e) {
-
-    if (e.code==="Enter"){
-        solve();
-    }
-})
+inputValue.addEventListener("keydown", function (e) {
+  if (e.code === "Enter") {
+    solve();
+  }
+});
 
 //Event for detele histroy or select histroy
-history_element.addEventListener("click",(event)=>{
-    const TARGET_BTN=event.target;
-    if(TARGET_BTN.name=="delete"){
-        let target = document.getElementById(TARGET_BTN.id);
-        target.parentElement.remove();
-    } else {
-        screen.value=TARGET_BTN.id;
-        ans_screen.value=TARGET_BTN.value
-        
-    }
-})
+history_element.addEventListener("click", (event) => {
+  const TARGET_BTN = event.target;
+  if (TARGET_BTN.name == "delete") {
+    let target = document.getElementById(TARGET_BTN.id);
+    target.parentElement.remove();
+  } else {
+    inputValue.value = TARGET_BTN.id;
+    outputValue.value = TARGET_BTN.value;
+  }
+});
 
 //Event for detele variable or select value of variable
-show_variables.addEventListener('click',(event)=>{
-    const TARGET_BTN=event.target;
-    if(TARGET_BTN.name=="deletevar"){
-        let target=document.getElementById(TARGET_BTN.id);
-        let targetvalue=document.getElementById(TARGET_BTN.id).value;
-        variables.delete(targetvalue)
-        target.parentElement.remove();
-    }else if(TARGET_BTN.name=="putvar"){
-        screen.value+=TARGET_BTN.id;
-    }
-})
-
+show_variables.addEventListener("click", (event) => {
+  const TARGET_BTN = event.target;
+  if (TARGET_BTN.name == "deletevar") {
+    let target = document.getElementById(TARGET_BTN.id);
+    variables.delete(target.id);
+    target.parentElement.remove();
+  } else if (TARGET_BTN.name == "putvar") {
+    inputValue.value += TARGET_BTN.id;
+  }
+});
 
 //function for show variables
-function showVar(){
-    if(!showvarbtn){
-        show_variables.style.display="block";
-        showvarbtn=true;
-    }else {
-        show_variables.style.display="none";
-        showvarbtn=false;
-    }
+function showVariables() {
+  if (!showVarBtn) {
+    show_variables.style.display = "block";
+    showVarBtn = true;
+  } else {
+    show_variables.style.display = "none";
+    showVarBtn = false;
+  }
 }
 
 //function for Error display
-function showErrorInput(){
-    errorSection.style.display="none";
+function showErrorInput() {
+  errorSection.style.display = "none";
 }
--- parser.js
+++ parser.js
@@ -1,37 +1,43 @@
 //Parser Function
 function evaluate(expression) {
-  try{
+  try {
     let tokens = expression.split("");
     let values = [];
-    let ops = [];
+    let operators = [];
     for (let i = 0; i < tokens.length; i++) {
       if (tokens[i] == " ") {
         continue;
       }
+      debugger
       if (tokens[i] >= "0" && tokens[i] <= "9") {
         let sbuf = "";
-        while (i < tokens.length &&((tokens[i] >= "0" && tokens[i] <= "9") || tokens[i] == ".")
+        while (i < tokens.length && ((tokens[i] >= "0" && tokens[i] <= "9") || tokens[i] == ".")
         ) {
           sbuf = sbuf + tokens[i++];
         }
         values.push(parseFloat(sbuf));
         i--;
       } else if (tokens[i] == "(") {
-        ops.push(tokens[i]);
+        operators.push(tokens[i]);
       } else if (tokens[i] == ")") {
-        while (ops[ops.length-1] != "(" && ops.length!=0) {
-          values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+        if (!operators.includes("(")) {
+          throw new Exception()
         }
-        ops.pop();
-        let getOp = ops.pop();
-        ops.push(getOp);
-        if (getOp == "cos" || getOp == "sin" || getOp == "tan" ||getOp == "sqrt") {
-          let newPushvalue = applyUnOp(ops.pop(), values.pop());
+        while (operators[operators.length - 1] != "(" && operators.length != 0) {
+          values.push(applyOp(operators.pop(), values.pop(), values.pop()));
+        }
+        operators.pop();
+        let getOp = operators.pop();
+        // if (getOp == undefined) {
+        //   throw new Exception()
+        // }
+        operators.push(getOp);
+        if (getOp == "cos" || getOp == "sin" || getOp == "tan" || getOp == "sqrt") {
+          let newPushvalue = applyUnOp(operators.pop(), values.pop());
           values.push(newPushvalue);
         }
       }
-      else if ( tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/") 
-      {
+      else if (tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/") {
         if (i == 0 && (tokens[i] == "-" || tokens[i] == "+")) {
           let sbuf = tokens[i];
           i++;
@@ -53,84 +59,83 @@ function evaluate(expression) {
             i--;
           }
         } else {
-          while (ops.length > 0 &&values.length>1 && hasPrecedence(tokens[i], ops[ops.length - 1])) 
-          {
-            values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+          while (operators.length > 0 && values.length > 1 && hasPrecedence(tokens[i], operators[operators.length - 1])) {
+            values.push(applyOp(operators.pop(), values.pop(), values.pop()));
           }
-          ops.push(tokens[i]);
+          operators.push(tokens[i]);
         }
       } else if (tokens[i] == "c" || tokens[i] == "t" || tokens[i] == "s") {
         let newValue = "";
         while (i < tokens.length && tokens[i] != "(") {
           newValue = newValue + tokens[i++];
         }
-        ops.push(newValue);
+        operators.push(newValue);
         i--;
       } else if (tokens[i] == "^") {
-        ops.push("^");
+        operators.push("^");
       }
     }
-    while (ops.length > 0) {
-      let getOp = ops.pop();
-      if(getOp!=undefined){
-      ops.push(getOp);
-      if (getOp != "cos" && getOp != "sin" && getOp != "tan" && getOp != "sqrt") {
-        values.push(applyOp(ops.pop(), values.pop(), values.pop()));
-      } else {
-        let newPushvalue = applyUnOp(ops.pop(), values.pop());
-        values.push(newPushvalue);
-      }}
+    while (operators.length > 0) {
+      let getOp = operators.pop();
+      if (getOp != undefined) {
+        operators.push(getOp);
+        if (getOp != "cos" && getOp != "sin" && getOp != "tan" && getOp != "sqrt") {
+          values.push(applyOp(operators.pop(), values.pop(), values.pop()));
+        } else {
+          let newPushvalue = applyUnOp(operators.pop(), values.pop());
+          values.push(newPushvalue);
+        }
+      }
     }
-    if(values.length==1)
-    return values.pop();
+    if (values.length == 1)
+      return values.pop();
     else
+      return "Error"
+  }
+  catch (Exception) {
     return "Error"
   }
-  catch(Exception){
-    return "Error"
+}
+function hasPrecedence(op1, op2) {
+  if (op2 == "(" || op2 == ")") {
+    return false;
   }
+  if ((op1 == "*" || op1 == "/") && (op2 == "+" || op2 == "-")) {
+    return false;
+  } else {
+    return true;
   }
-  function hasPrecedence(op1, op2) {
-    if (op2 == "(" || op2 == ")") {
-      return false;
-    }
-    if ((op1 == "*" || op1 == "/") && (op2 == "+" || op2 == "-")) {
-      return false;
-    } else {
-      return true;
-    }
-  }
+}
 //function for binary operation
 function applyOp(op, b, a) {
-    switch (op) {
-      case "+":
-        return a + b;
-      case "-":
-        return a - b;
-      case "*":
-        return a * b;
-      case "^":
-        return Math.pow(a, b);
-      case "/":
-        if (b == 0) {
+  switch (op) {
+    case "+":
+      return a + b;
+    case "-":
+      return a - b;
+    case "*":
+      return a * b;
+    case "^":
+      return Math.pow(a, b);
+    case "/":
+      if (b == 0) {
         return "Infinity";
-        }
-        return parseFloat(a / b, 10);
-    }
-    return "NaN";
+      }
+      return parseFloat(a / b, 10);
   }
-  //function for Uniary operation
-  function applyUnOp(op, b) {
-    switch (op) {
-      case "cos":
-        return Math.cos(b);
-      case "sin":
-        return Math.sin(b);
-      case "tan":
-        return Math.tan(b);
-      case "sqrt":
-        return Math.sqrt(b);
-    }
-    return "NaN";
+  return "NaN";
+}
+//function for Uniary operation
+function applyUnOp(op, b) {
+  switch (op) {
+    case "cos":
+      return Math.cos(b);
+    case "sin":
+      return Math.sin(b);
+    case "tan":
+      return Math.tan(b);
+    case "sqrt":
+      return Math.sqrt(b);
   }
-
+  return "NaN";
+}
